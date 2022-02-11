21f041b5862af8db9f6a18f478b696f24a494065
Черногорцева Полина <cernogorcevapolina@MacBook-Pro-Cernogorceva.local>
2022-02-11 15:23:43 +0300

    all1345678

--- db/seeders/20220204131543-user.js
+++ db/seeders/20220204131543-user.js
@@ -12,42 +12,64 @@ module.exports = {
          * }], {});
          */
         await queryInterface.bulkInsert('Users', [
-            {
-                name: 'Polina',
-                email: '1@1',
-                password: '$2a$10$9mEpp4RJ0mBUEjhY/0AunezhqXvs0UVRDAfJ9xr73FtFAgASLqUKa',//123
-                createdAt: new Date(),
-                updatedAt: new Date()
-            },
-            {
-                name: 'Egor ',
-                email: '2@2',
-                password: '$2a$10$hU/xg9.FXY7W23AhpEz/TeBd1Y7yEepQAEyDFgwvE1aRE5K/fLnQW',//1234
-                createdAt: new Date(),
-                updatedAt: new Date()
-            },
-            {
-                name: 'Marsel ',
-                email: '3@3',
-                password: '$2a$10$wkcr.Oqi3j4UnZVbqqyfAOANG8Nllpl6R9BMi58qwwTPMx1LmfDA2',//12345
-                createdAt: new Date(),
-                updatedAt: new Date()
-            },
-            {
-                name: 'Yriy ',
-                email: '4@4',
-                password: '$2a$10$4glpY4C6ANudGbsO51sU0uOnvkPO9m63f.37hNhmboUmDgt1JW8Y6',//123456
-                createdAt: new Date(),
-                updatedAt: new Date()
-            },
-            {
-                name: 'Anton ',
-                email: '5@5',
-                password: '$2a$10$p/AGiMO3gGm6iH47spePRu5lBCfBCLC/d7W1FYOaPodJxT9U0ILdi',//1234567
-                createdAt: new Date(),
-                updatedAt: new Date()
-            }
-        ], {});
+            await queryInterface.bulkInsert('Users', [
+                {
+                    name: 'Polina',
+                    email: '1@1',
+                    password: '$2a$10$9mEpp4RJ0mBUEjhY/0AunezhqXvs0UVRDAfJ9xr73FtFAgASLqUKa',//123
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: 'Egor ',
+                    email: '2@2',
+                    password: '$2a$10$hU/xg9.FXY7W23AhpEz/TeBd1Y7yEepQAEyDFgwvE1aRE5K/fLnQW',//1234
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: 'Marsel ',
+                    email: '3@3',
+                    password: '$2a$10$wkcr.Oqi3j4UnZVbqqyfAOANG8Nllpl6R9BMi58qwwTPMx1LmfDA2',//12345
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: 'Yriy ',
+                    email: '4@4',
+                    password: '$2a$10$4glpY4C6ANudGbsO51sU0uOnvkPO9m63f.37hNhmboUmDgt1JW8Y6',//123456
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: 'Anton ',
+                    email: '5@5',
+                    password: '$2a$10$p/AGiMO3gGm6iH47spePRu5lBCfBCLC/d7W1FYOaPodJxT9U0ILdi',//1234567
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: 'Геннадий',
+                    email: '6@6',
+                    password: '$2a$10$RY.HbXL5f09iW6ISdRzb0e8MZmmtF36sOuOd30gosj5QXzGD.mgSC',//12345678
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: 'Феликс ',
+                    email: '7@7',
+                    password: '$2a$10$WGXKYbjFMEuRwJczUM.t0uSmiJ0m3ZdPguQ8H9eZNdydk3LHLlZEy',//123456789
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: 'Марат ',
+                    email: '8@8',
+                    password: '$2a$10$AIQDI/9Y/ZPLzUZFFkNEJ.Tui92lXTrqUhMOmf2k41Nd3wvjaeKru',//1234567890
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                }
+            ], {});
 
 
     },
--- db/seeders/20220205133804-profile.js
+++ db/seeders/20220205133804-profile.js
@@ -14,42 +14,66 @@ module.exports = {
 
         await queryInterface.bulkInsert('Profils', [
             {
-                name: 'Polina',
+                name: 'Полина',
                 user_id: '1',
-                description: 'люблю читать книги и чинить телефоны',
+                description: 'Люблю читать книги и чинить телефоны',
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
-
             {
-                name: 'Egor',
+                name: 'Егор',
                 user_id: '2',
-                description: 'люблю играть на гитаре',
+                description: 'Люблю играть на гитаре',
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
-                name: 'Marsel',
+                name: 'Марсель',
                 user_id: '3',
-                description: 'люблю петь и придумывать тосты',
+                description: 'Люблю петь и придумывать тосты',
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
-
             {
-                name: 'Yriy',
+                name: 'Юрий',
                 user_id: '4',
-                description: 'люблю показывать фокусы',
+                description: 'Люблю показывать фокусы',
+                img: 'blob:https://web.telegram.org/556a85f2-8d4e-4a00-bd24-1395aee514ce',
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
-                name: 'Anton',
+                name: 'Антон',
                 user_id: '5',
-                description: 'люблю помогать людям бескорыстно',
+                description: 'Люблю помогать людям бескорыстно, могу переустановить винду',
+                img: 'https://ca.slack-edge.com/T02NH8VRBAS-U02P66FU54H-8e28b2da15dc-512',
                 createdAt: new Date(),
                 updatedAt: new Date()
-            }
+            },
+            {
+                name: 'Геннадий',
+                user_id: '6',
+                description: 'Описание: За тепло в Вашем доме!',
+                img: 'http://filurin.ru/wp-content/uploads/2015/09/4sokol.jpg',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Феликс',
+                user_id: '7',
+                description: 'Даю свое тепло, чиню разбитые сердечки',
+                img: 'https://pbs.twimg.com/media/CaNONveWQAAtP0o.jpg:large',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Марат Прохоров',
+                user_id: '8',
+                description: 'Ремонт батарей, в доме чисто в два раза быстрей!',
+                img: 'file:///home/user/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/s-bbe331b52a73838d1c074a2337aa38b289b388f0.jpg',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
         ], {});
     },
 
